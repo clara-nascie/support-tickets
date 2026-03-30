ed245afa99529c30cd515542f71ac393a88da30a
Clara Nascimento <clara.nasc13@gmail.com>
2026-03-30 19:50:22 -0300

    feat: implement HTTP server with JSON and route handling middlewares

---
+++ src/middlewares/routeHandler.js
@@ -0,0 +1,23 @@
+// resposável por lidar com as rotas 
+
+//importando as rotas 
+import { routes } from "../routes/index.js";
+
+//função que vai lidar com as rotas 
+export function routeHandler(req, res) {
+   //procurando a rota que corresponde ao método e ao caminho da requisição
+    const route = routes.find((route) => {
+        //retorna true se a rota for encontrada
+        return route.method === req.method && route.path === req.url
+    })
+
+    // se route existe 
+    if (route) { 
+        //executa a função controller (criado com sucesso!) se a rota for encontrada
+        return route.controller(req,res) 
+    }
+
+    //retorna 404 se a rota não for encontrada
+    return res.writeHead(404).end("Rota não encontrada")
+    
+}
--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import http from "node:http"
 import { jsonHandler } from "./middlewares/jsonHandler.js"
+import { routeHandler } from "./middlewares/routeHandler.js"
 
 async function listener(req, res) {
    //chamando o middleware de json de forma assíncrona
    await jsonHandler(req, res)
 
-   console.log(req.body)
+   //chamando o middleware de rotas
+   routeHandler(req, res)
 
    // Enviando uma resposta para que o Insomnia não fique aguardando eternamente
    return res.end(JSON.stringify({ message: "Servidor rodando com sucesso!" }))
