04b03b910414dd37d5031ec68027f9597641e177
Clara Nascimento <clara.nasc13@gmail.com>
2026-03-31 11:17:17 -0300

    feat: implement create controller for ticket generation

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,4 +1,22 @@
+//importando o crypto para gerar id
+import {randomUUID} from "node:crypto"
+
 //importando o banco de dados, quando entre {} a ordem de chamada nao importa 
 export function create({req, res}) {
-   return res.end("Criado com sucesso")
+   //desestruturando o body da requisicao
+   const {equipment, description, user_name} = req.body
+
+   //criando o ticket/objeto
+   const ticket = {
+      id: randomUUID(),
+      equipment,
+      description,
+      user_name,
+      status: "open",
+      created_at: new Date(),
+      updated_at: new Date(),
+   }
+
+   //quando der certo, retorna o ticket criado
+   return res.end(JSON.stringify(ticket))
 }
