d608f656ee46db5490f613501cbb4ae729c5884e
Clara Nascimento <clara.nasc13@gmail.com>
2026-03-31 16:47:19 -0300

    feat: implement ticket update controller and add corresponding route

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,5 +1,6 @@
 import { create } from "../controllers/tickets/create.js"
 import { index } from "../controllers/tickets/index.js"
+import { update } from "../controllers/tickets/update.js"
 
 export const tickets = [
     {
@@ -16,5 +17,13 @@ export const tickets = [
         path: '/tickets', //caminho da rota
         //função que será executada quando a rota for acessada
         controller: index,
+    },
+
+    {
+        //criando a rota de tickets para listar todos os tickets
+        method: 'PUT', //método http
+        path: '/tickets/:id', //caminho da rota
+        //função que será executada quando a rota for acessada
+        controller: update,
     },
 ]
